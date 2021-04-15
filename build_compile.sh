3b50c3f809dda6ba85104f255254927979637648
brunoais <brunoaiss@gmail.com>
2021-04-15 09:13:08 +0100

    Fix build args not being sent to the container build

--- build_compile.sh
+++ build_compile.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-. ./.source.sh
+. .source.sh
+
+HIST_COMMAND_ARG=
+
+[[ $HIST_COMMAND ]] && HIST_COMMAND_ARG=--build-arg "HIST_COMMAND=$HIST_COMMAND"
+
 
 docker build \
 -f 'Dockerfile_prepare' \
 --target scrcpyCompilePrepare \
---build-arg "USER_NAME" \
---build-arg "USER_ID" \
---build-arg "HIST_COMMAND" \
+--build-arg "USER_NAME=$USER_NAME" \
+--build-arg "USER_ID=$USER_ID" \
+$HIST_COMMAND_ARG \
 -t "$PART_1_TAG" \
 .
--- build_compiled.sh
+++ build_compiled.sh
@@ -4,14 +4,18 @@
 
 if ./build_compile.sh; then
 
+	[[ $HIST_COMMAND ]] && HIST_COMMAND_ARG=--build-arg "HIST_COMMAND=$HIST_COMMAND"
+	[[ $COMPILE_SERVER ]] && COMPILE_SERVER_ARG=--build-arg "COMPILE_SERVER=$COMPILE_SERVER"
+
 	docker build \
 	-f 'Dockerfile_compile' \
 	--target scrcpyCompile \
-	--build-arg "USER_NAME" \
-	--build-arg "USER_ID" \
-	--build-arg "HIST_COMMAND" \
-	--build-arg "COMPILE_SERVER" \
-	--build-arg "PART_1_TAG" \
+	--build-arg "USER_NAME=$USER_NAME" \
+	--build-arg "USER_ID=$USER_ID" \
+	$HIST_COMMAND_ARG \
+	$COMPILE_SERVER_ARG \
+	--build-arg "COMPILE_SERVER=$COMPILE_SERVER" \
+	--build-arg "PART_1_TAG=$PART_1_TAG" \
 	-t "$PART_2_TAG" \
 	scrcpy
 else
